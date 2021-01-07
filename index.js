c82274ad5900c5b2c6f87e65aa4ede2396a5053a
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2021-01-06 23:41:22 -0500

    fixed api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const server = require('./api/server.js')
 
-const PORT = process.env.PORT || 4500
+const PORT = process.env.PORT || 4800
 
 server.listen(PORT, () => {
   console.log(`\n Listening on port ${PORT}... \n`)
