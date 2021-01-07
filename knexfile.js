f478f079c9f05d891ca1887f12a0e3eaad3ba7c9
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2021-01-06 22:23:20 -0500

    fixing bug api

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
     },
   },
     migrations: {
-      directory: "./database/migrations",
+      directory: "./database/migrations"
     },
     seeds: {
       directory: './database/seeds'
@@ -36,6 +36,22 @@ module.exports = {
   //     directory: "./database/seeds",
   //   }
   // },
+  
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
 
   production: {
     client: 'postgresql',
