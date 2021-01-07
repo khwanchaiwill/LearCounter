ac6f6f71f5873e88c82a3c237daffa29b465aed3
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2021-01-06 22:47:40 -0500

    fixing bug api

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 // Update with your config settings.
 require("dotenv").config();
-const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/count"
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/count";
 // const { pgConnection } = require("./config/vars")
 
 module.exports = {
@@ -36,25 +36,9 @@ module.exports = {
   //     directory: "./database/seeds",
   //   }
   // },
-  
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: pgConnection,
     pool: {
       min: 2,
